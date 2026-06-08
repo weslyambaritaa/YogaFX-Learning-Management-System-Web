61d85965dc699babd586fb97a6223307e98172c2
wesly <weslyambarita4@gmail.com>
2026-06-08 14:20:01 +0700

    first commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,12 @@
+import "../css/app.css";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createRoot } from "react-dom/client";
+
+createInertiaApp({
+    resolve: (name) =>
+        resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob("./Pages/**/*.jsx")),
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+});
