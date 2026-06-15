20c8a4a7c7ebd90c96ce8d2b4a4bd9dbc8b0af26
MosesSimangunsong <142555927+MosesSimangunsong@users.noreply.github.com>
2026-06-15 17:44:02 +0800

    fix file max upload ebook

--- app/Support/UploadConstraints.php
+++ app/Support/UploadConstraints.php
@@ -7,6 +7,9 @@ class UploadConstraints
     public const MAX_FILE_SIZE_KB = 10240;
     public const MAX_FILE_SIZE_MB = 10;
 
+    public const EBOOK_MAX_FILE_SIZE_KB = 102400;
+    public const EBOOK_MAX_FILE_SIZE_MB = 100;
+
     public const LESSON_WORKBOOK_MAX_FILE_SIZE_KB = 102400;
     public const LESSON_WORKBOOK_MAX_FILE_SIZE_MB = 100;
 
