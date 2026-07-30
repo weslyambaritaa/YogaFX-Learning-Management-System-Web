bfadfd074fcfca398741eb23f04ef3fa54bb6007
wesly <weslyambarita4@gmail.com>
2026-07-30 14:48:23 +0700

    update

--- resources/js/Components/student/WelcomeToYogaFXDialog.jsx
+++ resources/js/Components/student/WelcomeToYogaFXDialog.jsx
@@ -26,7 +26,8 @@ export default function WelcomeToYogaFXDialog({ open, onOpenChange }) {
                         />
                         <div className="space-y-2">
                             <DialogTitle className="text-2xl font-semibold text-white sm:text-3xl">
-                                Welcome to YogaFX.
+                                Welcome to Yoga
+                                <span className="text-[#c00000]">FX</span>.
                             </DialogTitle>
                         </div>
                     </DialogHeader>
