c5d79ba2357e155c63d1a755364ca39f146e3e0f
MosesSimangunsong <142555927+MosesSimangunsong@users.noreply.github.com>
2026-08-12 14:56:48 +0800

    Update PublicFlowLayout.jsx

--- resources/js/Layouts/PublicFlowLayout.jsx
+++ resources/js/Layouts/PublicFlowLayout.jsx
@@ -331,14 +331,14 @@ export default function PublicFlowLayout({
 
                                 <h2 className="mx-auto mt-10 max-w-2xl text-2xl font-bold leading-tight text-white sm:text-3xl">
                                     We Are Thrilled That You Are Joining Mr.
-                                    Ian&apos;s {renderYogaFXTitle(title)}{" "}
-                                    Practical MasterClass In Beautiful Bali
+                                    Ian&apos;s {renderYogaFXTitle(title)} In
+                                    Beautiful Bali
                                 </h2>
 
                                 <p className="mx-auto mt-6 max-w-xl text-base font-semibold italic leading-relaxed text-white sm:text-lg">
                                     Your Enrollment Starts Now
                                 </p>
-                            
+
                                 <button
                                     type="button"
                                     onClick={startFinalCountdown}
